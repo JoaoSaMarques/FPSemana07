62152019b71dab3975175763deb45c72a24e2c38
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-12-20 12:48:11 +0000

    add enviar_mensagem on each class

--- mensagem.py
+++ mensagem.py
@@ -2,15 +2,23 @@ class Mensagem:
     def __init__(self):
         pass
     
-    
 class Email(Mensagem):
     def __init__(self):
         super().__init__()
+        
+    def enviar_mensagem(self):
+        print("Mensagem enviada por e-mail")
         
 class SMS(Mensagem):
     def __init__(self):
         super().__init__()
         
+    def enviar_mensagem(self):
+        print("Mensagem enviada por SMS")
+        
 class NotificacaoPush(Mensagem):
     def __init__(self):
         super().__init__()
+        
+    def enviar_mensagem(self):
+        print("Mensagem enviada por notificação push")
